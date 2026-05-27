adcd0754045621d35ad7cf73d50719501a9293eb
kvbek <jakub.s.marciniak@gmail.com>
2026-05-27 21:28:11 +0200

    Updated analysis, plots  and created new charts

--- src/utils/plots.py
+++ src/utils/plots.py
@@ -1,6 +1,50 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from IPython.display import clear_output
+from pypalettes import load_cmap
+from pyfonts import load_font
+
+# Load custom fonts
+poppins_light = load_font("https://github.com/google/fonts/blob/main/ofl/poppins/Poppins-Light.ttf?raw=true", danger_not_verify_ssl=True)
+poppins_bold = load_font("https://github.com/google/fonts/blob/main/ofl/poppins/Poppins-Bold.ttf?raw=true", danger_not_verify_ssl=True)
+
+# Font size
+title_fontsize = 16
+subtitle_fontsize = 13
+tick_fontsize = 9
+label_fontsize = 11
+
+# Load color palettes
+cmap = load_cmap("Semicossyphus_pulcher", reverse=True)
+cmap_unreversed = load_cmap("Semicossyphus_pulcher", reverse=False)
+cmap_x85 = load_cmap("X85", reverse=True)
+
+def style_plot_fonts(ax, font_obj=poppins_light, tick_size=tick_fontsize, label_size=label_fontsize, remove_spines=True):
+    """
+    Applies custom font styling to a matplotlib axis.
+    """
+    # 1. Customize tick parameters (remove lengths, set base size)
+    ax.tick_params(axis='both', labelsize=tick_size, length=0, rotation=0)
+
+    # 2. Set font for tick labels
+    for tick in ax.get_xticklabels():
+        tick.set_fontproperties(font_obj)
+        tick.set_fontsize(tick_size)
+        
+    for tick in ax.get_yticklabels():
+        tick.set_fontproperties(font_obj)
+        tick.set_fontsize(tick_size)
+
+    # 3. Set font for axis labels (only if they exist)
+    if ax.get_xlabel():
+        ax.set_xlabel(ax.get_xlabel(), fontproperties=font_obj, fontsize=label_size)
+    
+    if ax.get_ylabel():
+        ax.set_ylabel(ax.get_ylabel(), fontproperties=font_obj, fontsize=label_size)
+
+    if remove_spines:
+        ax.spines[["top", "right"]].set_visible(False)
+
 
 class LivePlotCallback(tf.keras.callbacks.Callback):
     def on_train_begin(self, logs=None):
